0e10dea4e0174490b36dc2858b95968353974e0c
Shi Lei <shi_lei@massclouds.com>
2021-11-10 10:57:55 +0800

    platform: display resources

--- platform/test.c
+++ platform/test.c
@@ -16,14 +16,22 @@ test_platform(void)
 
     klist_iter_init_node(&bus->p->klist_devices, &iter, NULL);
     while ((n = klist_next(&iter))) {
+        int i;
         struct device_private *dev_prv = to_device_private_bus(n);
         struct device *dev = dev_prv->device;
+        struct platform_device *pdev = to_platform_device(dev);
         struct device_node *of_node = dev->of_node;
         struct property *property = of_node->properties;
-        printk("device: %s. of porperties:\n", of_node->full_name);
+        printk("device: %s.\n", of_node->full_name);
+        printk("  of porperties:\n");
         while (property) {
             printk("    [%s]\n", property->name);
             property = property->next;
+        }
+        printk("  resources (%u):\n", pdev->num_resources);
+        for (i = 0; i < pdev->num_resources; i++) {
+            printk("    resources (%lx, %lx):\n",
+                   pdev->resource[i].start, pdev->resource[i].end);
         }
     }
     klist_iter_exit(&iter);
