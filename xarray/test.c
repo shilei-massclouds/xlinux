664c9d60e15b17d89b182e2a1598bfe46458e29d
Shi Lei <shi_lei@massclouds.com>
2021-12-04 15:52:20 +0800

    xarray: add another test

--- xarray/test.c
+++ xarray/test.c
@@ -44,13 +44,11 @@ test_xas_store(struct xarray *xarray, pgoff_t offset, void *entry)
 }
 
 static int
-init_module(void)
+test1(void)
 {
     void *entry;
     struct xarray xarray;
     unsigned long dummy[2];
-
-    printk("module[test_xarray]: init begin ...\n");
 
     memset(&xarray, 0, sizeof(xarray));
 
@@ -75,6 +73,54 @@ init_module(void)
         printk(_RED("xa_store [0x1234]:%p error!"), entry);
         return -1;
     }
+
+    return 0;
+}
+
+static int
+test2(void)
+{
+    void *entry;
+    struct xarray xarray;
+    unsigned long dummy[2];
+
+    memset(&xarray, 0, sizeof(xarray));
+
+    if (test_xas_load(&xarray, 0x1234)) {
+        printk(_RED("xa_load 0x1234 error!"));
+        return -1;
+    }
+
+    if (test_xas_load(&xarray, 0)) {
+        printk(_RED("xa_load 0 error!"));
+        return -1;
+    }
+
+    entry = (void *) &dummy[0];
+    if (test_xas_store(&xarray, 0x1234, entry)) {
+        printk(_RED("xa_store [0x1234]:%p error!"), entry);
+        return -1;
+    }
+
+    entry = (void *) &dummy[1];
+    if (test_xas_store(&xarray, 0, entry)) {
+        printk(_RED("xa_store [0]:%p error!"), entry);
+        return -1;
+    }
+
+    return 0;
+}
+
+static int
+init_module(void)
+{
+    printk("module[test_xarray]: init begin ...\n");
+
+    printk("test1 ...\n");
+    test1();
+
+    printk("test2 ...\n");
+    test2();
 
     printk("module[test_xarray]: init end!\n");
     return 0;
